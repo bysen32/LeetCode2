6f11234b69c0141b59b6ac074f4f9daf49293598
bysen32 <bysen32@gmail.com>
2018-03-10 15:00:24 +0800

    update

--- scripts/readme.py
+++ scripts/readme.py
@@ -229,7 +229,7 @@ class Readme:
 
         with open(file_path, 'a') as f:
             f.write('## LeetCode Solution Table\n')
-            f.write('| ID | Title | Difficulty | JavaScript | Python | C++ | Java |\n')
+            f.write('| ID | Title | Difficulty | Python | C++ |\n')
             f.write('|:---:' * 7 + '|\n')
             table, table_item = table_instance
             # print(table)
